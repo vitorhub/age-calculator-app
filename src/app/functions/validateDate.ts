e4585351acdf6ff3565c3ede7f41c01eccb4e039
Vitor Falcao <joaofalcao.33@outlook.com>
2023-04-07 15:47:04 -0300

    i was able to validate a form

--- src/app/functions/validateDate.ts
+++ src/app/functions/validateDate.ts
@@ -2,35 +2,38 @@ export default function valDate(dateString: string) {
 
     console.log(dateString + "-----")
     let resposta = ''
-    let respostaAno: string | number = 'ANO CORRETO'
-    let respostaMes: string | number = 'MES CORRETO'
-    let respostaDia: string | number = 'DIA CORRETO'
-    let array: [string | number, string | number, string | number] =
-        [respostaDia, respostaMes, respostaAno]
+    let respostaAno: string | number = ''
+    let respostaMes: string | number = ''
+    let respostaDia: string | number = ''
+    let array: any = []
 
     // Parse the date parts to integers
     var parts = dateString.split("/");
     var dia = parseInt(parts[0], 10);
     var mes = parseInt(parts[1], 10);
     var ano = parseInt(parts[2], 10);
-    console.log(mes + "*"+ dia + "*" + ano )
+    console.log(dia + "*" + mes + "*" + ano)
 
     if (ano < 1923 || ano > 2023 || isNaN(ano)) {
-        array[2] = "Max 100" ;
+        array[2] = "Max 100 ou invalido";
     } else {
         array[2] = "ANO OK";
     }
 
     if (mes < 1 || mes > 12 || isNaN(mes)) {
-        array[1] = "MES ERRADO ou NaN"
-    } else if (dia < 1 || isNaN(dia)) {
-        array[0] = "dia menor que 1 ou NaN"
+        array[1] = "L24 mes invalido"
+    }else{
+        array[1] = "MES OK"
+    }
+    if (dia < 1 || isNaN(dia)) {
+        array[0] = "L27 dia invalido"
     } else {
         switch (mes) {
             case 1: case 3: case 5: case 7:
             case 8: case 10: case 12:
                 if (dia <= 31) {
                     resposta = "Data válida mes com 31 dias";
+                    array[0] = "31 DIAS OK";
                 } else {
                     resposta = "L:36 Max day 31";
                     array[0] = "L:37 Max day 31";
@@ -40,6 +43,7 @@ export default function valDate(dateString: string) {
             case 9: case 11:
                 if (dia <= 30) {
                     resposta = "L:30 Data válida";
+                    array[0] = "30 DIAS OK";
                 } else {
                     resposta = "L:43 Max day 30";
                     array[0] = "L:44 Max day 30";
@@ -49,6 +53,7 @@ export default function valDate(dateString: string) {
                 if ((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) {
                     if (dia <= 29) {
                         resposta = "Data válida é bisexto";
+                        array[0] = "29 DIAS OK";
                     } else {
                         resposta = "L:52 Max day 29";
                         array[0] = "L:53 Max day 29";
@@ -57,13 +62,13 @@ export default function valDate(dateString: string) {
                 else {
                     if (dia <= 28) {
                         resposta = "Data válida ano comum";
+                        array[0] = "28 DIAS OK";
                     } else {
                         resposta = "L:59 Max day 28";
                         array[0] = "L:60 Max day 28";
                     }
                 }
         }
-        return array
     }
-
+    return array
 }
